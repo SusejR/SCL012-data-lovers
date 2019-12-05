37c0dcb11deed162e57717abb39abd2a53744b91
SusejR <rodriguezbsusej@gmail.com>
2019-12-05 14:00:47 -0300

    logra mostrar en pantalla data

--- src/main.js
+++ src/main.js
@@ -1,49 +1,8 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
 
+import POKEMON from './data/pokemon/pokemon.js';
 
-
-import { POKEMON } from './data/pokemon/pokemon.js';
-
-console.log(POKEMON);
-
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
-
-
-
-// import { example } from './data.js';
-
-// console.log(example);
-
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+for (let i = 0; i < POKEMON.length; i++) {
+  const elemento = document.getElementById('cont');
+  elemento.innerHTML += `<p>${POKEMON[0].name}</p>
+   <p>${POKEMON[0].type}</p>`;
+}
