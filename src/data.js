fe3e0def77a5502d9ca383a9a2a10f7fca170018
camila-osores <camilaosoresa@gmail.com>
2019-12-05 14:05:05 -0300

    intentando hacer que muestre data

--- src/data.js
+++ src/data.js
@@ -2,9 +2,16 @@
 
 // esta es una función de ejemplo
 
-
+fetch(POKEMON)
 
 
 export const POKEMON = (POKEMON) => {
+
+  for(let i of POKEMON){
+    console.log(i);
+
+  }
+
   return 'POKEMON';
+
 };
