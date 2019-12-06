9e7f2292b929fae470e014f96cb7c4d96364b962
SusejR <rodriguezbsusej@gmail.com>
2019-12-06 10:59:54 -0300

    nada

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 
 import POKEMON from './data/pokemon/pokemon.js';
 
-for (let i = 0; i < POKEMON.length; i++) {
+
+//for (let i = 0; i < POKEMON.length; i++) {
   const elemento = document.getElementById('cont');
   elemento.innerHTML += `<p>${POKEMON[0].name}</p>
-   <p>${POKEMON[0].type}</p>`;
-}
+   <p>${POKEMON[0].type}</p>
+    <img src= "${POKEMON[0].img}">`;
+//}
