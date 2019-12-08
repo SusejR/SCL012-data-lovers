7076950557319219384f1bf0fd8329cdd58faae7
SusejR <rodriguezbsusej@gmail.com>
2019-12-08 13:08:36 -0300

    modificaciones en html

--- src/main.js
+++ src/main.js
@@ -1,31 +1,7 @@
-
 import POKEMON from './data/pokemon/pokemon.js';
-
-// console.log(POKEMON);
-// console.log(POKEMON[0].type);
-
-
-// let con = document.getElementById("con")
-// cont.innerHTML = POKEMON[0].next_evolution[0].name;
-
-for (const poke of POKEMON) {
-  console.log(poke.name)
-  if (poke.candy_count > 25) {
-    con.innerHTML += `
-    <p>${poke.name}</p>
-    <img src= "${poke.img}">
-    <p>${poke.next_evolution}</p>
-  `
-  }
+const pokemones = document.getElementById('pokemones');
+for (let i = 0; i < POKEMON.length; i+=1) {
+    pokemones.innerHTML +=`
+    <img src="${POKEMON[i].img}">
+    <p>"${POKEMON[i].name}"</p>`;
 }
-
-// for (let i=0; i< POKEMON.length; i++){
-//     // console.log(POKEMON[i].name);
-
-//     con.innerHTML += `
-//     <p>${POKEMON[i].name}</p>
-//     <img src= "${POKEMON[i].img}">
-
-//     `
-
-// }
