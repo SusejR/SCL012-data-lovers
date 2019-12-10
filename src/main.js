6326191580a3db5dc9a357d8af0a29d9293c861d
camila-osores <camilaosoresa@gmail.com>
2019-12-10 10:29:52 -0300

    modificando CSS

--- src/main.js
+++ src/main.js
@@ -2,22 +2,25 @@ import POKEMON from './data/pokemon/pokemon.js';
 
 let poke = document.getElementById("pokemones");
 for(let pok of POKEMON){
+  
   poke.innerHTML += `
     <div class="cajaPokemon" id="cajaPokemon">      
-    <img class="fotos" src="${pok.img}">
-    <div class="descripcion">
-    <p class="nombres">${pok.name}</p><br>
-    <p> Tipo: ${pok.type}</p>
-    <p> Altura: ${pok.height}</p>
-    <p> Peso: ${pok.weight}</p>
+      <img class="fotos" src="${pok.img}">
+      <div class="descripcion">
+      <p class="nombres">${pok.name}</p><br>
+      <p> # ${pok.num}</p>
+      <p> Tipo: ${pok.type}</p>
+      <p> Altura: ${pok.height}</p>
+      <p> Peso: ${pok.weight}</p>
+    
+      <span>
+        <p class="nombres"> Características </p><br>
+        <p> Caramelos para evolucionar: ${pok.candy_count}</p>
+        <p> Huevo: ${pok.egg}</p>
+        <p> Posibilidad de aparición: ${pok.spawn_chance}</p>
+        <p> Tiempo de generación: ${pok.spawn_time}</p>
+        <p> Debilidades: ${pok.weaknesses}</p>
+      </span>
     </div>
     `
-}
-
-
-// const pokemones = document.getElementById('pokemones');
-// for (let i = 0; i < POKEMON.length; i+=1) {
-//     pokemones.innerHTML +=`
-//     <img src="${POKEMON[i].img}">
-//     <p>${POKEMON[i].name}</p>`;
-// }
+} 
