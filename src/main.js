77577342d9a212180467f81725a881353f7b6512
camila-osores <camilaosoresa@gmail.com>
2019-12-11 10:02:09 -0300

    modificacion CSS responsive celu

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import POKEMON from './data/pokemon/pokemon.js';
 
 let poke = document.getElementById("pokemones");
-
 const tipos = document.getElementById("types");
 
 tipos.onchange = () => {
@@ -10,31 +9,34 @@ tipos.onchange = () => {
  showCards(filtrados);
  }
  
-
- 
 // Aqui empiezan las funciones
 
 const showCards = (data) => {
+  
   data.forEach(pok => {
+    let debilidades = pok.weaknesses.slice();
+    // console.log(variable.join(" "))
+
     poke.innerHTML += `
       <div class="cajaPokemon" id="cajaPokemon">      
-      <img class="fotos" src="${pok.img}">
-      <div class="descripcion">
-      <p class="nombres">${pok.name}</p><br>
-      <p> Tipo: ${pok.type}</p>
-      <p> Altura: ${pok.height}</p>
-      <p> Peso: ${pok.weight}</p>
+        <img class="fotos" src="${pok.img}">
+        <div class="descripcion">
+        <p class="nombres">${pok.name}</p><br>
+        <p> # ${pok.num}</p>
+        <p> Tipo: ${pok.type}</p>
+        <p> Altura: ${pok.height}</p>
+        <p> Peso: ${pok.weight}</p>
       
-  <span>
-        <p class="nombres"> Características </p><br>
-        <p> Caramelos para evolucionar: ${pok.candy_count}</p>
-        <p> Huevo: ${pok.egg}</p>
-        <p> Posibilidad de aparición: ${pok.spawn_chance}</p>
-        <p> Tiempo de generación: ${pok.spawn_time}</p>
-        <p> Debilidades: ${pok.weaknesses}</p>
-      </span>
-   </div>
-`
+        <span>
+          <p class="nombres">${pok.name}</p><br>
+          <p> Caramelos para evolucionar: ${pok.candy_count ? pok.candy_count : "No Evoluciona"}</p>
+           ${pok.egg == "Not in Eggs"? "" :"<p> Huevo:"+pok.egg+ "</p>"}
+          <p> Posibilidad de aparición: ${pok.spawn_chance}</p>
+          <p> Tiempo de generación: ${pok.spawn_time}</p>
+          <p> Debilidades: ${debilidades.join(", ")}</p>
+        </span>
+      </div>
+      `
   });
 }
 
